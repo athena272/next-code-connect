49ec474e33cb3632cd031c413d4c09772dcbc932
athena272 <guilhermera272@gmail.com>
2024-07-21 21:38:49 -0300

    Update index.tsx

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,11 +1,12 @@
 import styles from './Aside.module.scss'
 import Image from 'next/image'
+import logo from './logo.png'
 
 export default function Aside() {
     return (
         <aside className={styles.aside}>
             {/* <img src="/logo.png" alt="Logo da Code Connect" /> */}
-            <Image src={'/logo.png'} alt="Logo da Code Connect" width={128} height={40} />
+            <Image src={logo} alt="Logo da Code Connect" width={128} height={40} loading='eager' />
         </aside>
     )
 }

0ad52855abb1ae034e4c24ee349cc9901229bd69
athena272 <guilhermera272@gmail.com>
2024-07-23 23:40:32 -0300

    Update index.tsx

--- src/components/CardPost/index.tsx
+++ src/components/CardPost/index.tsx
@@ -1,18 +1,31 @@
 import Image from "next/image"
+import Avatar from "../Avatar"
+import { Post } from "@/types/Post"
 
-export default function CardPost({ post }) {
+type CardPostProps = {
+    post: Post,
+}
+
+export default function CardPost({ post }: CardPostProps) {
     return (
         <article>
             <header>
                 <figure>
-                    <Image />
+                    <Image
+                        src={post.cover}
+                        alt={`Um ótimo post`}
+                    />
                 </figure>
             </header>
             <section>
-                titulo
-                texto
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
             </section>
             <footer>
+                <Avatar
+                    imageSrc={post.author.avatar}
+                    name={post.author.name}
+                />
             </footer>
         </article>
     )

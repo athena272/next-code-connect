00f5726aab550bd9bbe225027bca13124c90bbdc
athena272 <guilhermera272@gmail.com>
2024-07-23 23:41:55 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import CardPost from "@/components/CardPost";
 import { Post } from "@/types/Post";
-import postsData from './posts.json'
+import { posts as POSTS } from './posts.json'
 
 export default function Home() {
-  const posts: Post[] = postsData.posts
+  const posts: Post[] = POSTS
 
   return (
     <main>
-      <CardPost post={posts[0]}/>
+      <CardPost post={posts[0]} />
     </main>
   );
 }

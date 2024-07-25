db5587c5e57d7b714cd28892968d531e8e28424d
athena272 <guilhermera272@gmail.com>
2024-07-25 08:30:52 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,49 @@
+'use client';
+
 import CardPost from "@/components/CardPost";
+import Pagination from "@/components/Pagination";
 import { Post } from "@/types/Post";
 import { getAllPosts } from "@/helper/functions";
 import styles from './page.module.scss'
+import { useEffect, useState } from "react";
 
-export default async function Home() {
-  const posts: Post[] = await getAllPosts()
+const POSTS_PER_PAGE = 4
+
+export default function Home() {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const fetchPosts = async () => {
+    const allPosts = await getAllPosts();
+    setPosts(allPosts);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const currentPosts = posts.slice((currentPage - 1) * POSTS_PER_PAGE, currentPage * POSTS_PER_PAGE);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
 
   return (
     <main className={styles.grid}>
       {
-        posts.map(post => (
+        currentPosts.map(post => (
           <CardPost
             key={post.id}
             post={post}
           />
         ))
       }
+      <Pagination
+        totalPosts={posts.length}
+        postsPerPage={POSTS_PER_PAGE}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+      />
     </main>
   );
 }

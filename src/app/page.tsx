0a9d0b6178464338db749b99748eba874a9ad684
athena272 <guilhermera272@gmail.com>
2024-07-25 07:57:18 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,7 @@
 import CardPost from "@/components/CardPost";
 import { Post } from "@/types/Post";
-import logger from '@/logger'
+import { getAllPosts } from "@/helper/functions";
 import styles from './page.module.scss'
-
-async function getAllPosts() {
-  try {
-    const response = await fetch('https://athena272.github.io/next-code-connect/posts.json')
-    if (!response.ok) {
-      throw new Error('Falha na rede');
-    }
-    
-    logger.info('Posts obtidos com sucesso')
-    const { posts } = await response.json();
-    return posts
-
-  } catch (error: any) {
-    logger.error('Ops, algo correu mal: ' + error.message)
-    return []
-  }
-
-
-
-
-}
 
 export default async function Home() {
   const posts: Post[] = await getAllPosts()

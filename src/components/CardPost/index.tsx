a0acb572d388f2e8037d67e8175c9dbc9ff2187c
athena272 <guilhermera272@gmail.com>
2024-07-25 22:38:55 -0300

    Update index.tsx

--- src/components/CardPost/index.tsx
+++ src/components/CardPost/index.tsx
@@ -2,6 +2,7 @@ import Image from "next/image"
 import Avatar from "../Avatar"
 import { Post } from "@/types/Post"
 import styles from './CardPost.module.scss'
+import Link from "next/link"
 
 type CardPostProps = {
     post: Post,
@@ -9,27 +10,29 @@ type CardPostProps = {
 
 export default function CardPost({ post }: CardPostProps) {
     return (
-        <article className={styles.card}>
-            <header className={styles.header}>
-                <figure>
-                    <Image
-                        src={post.cover}
-                        alt={`Capa do post de titulo: ${post.title}`}
-                        width={438}
-                        height={133}
+        <Link href={`/posts/${post.slug}`} passHref className={styles.link}>
+            <article className={styles.card}>
+                <header className={styles.header}>
+                    <figure>
+                        <Image
+                            src={post.cover}
+                            alt={`Capa do post de titulo: ${post.title}`}
+                            width={438}
+                            height={133}
+                        />
+                    </figure>
+                </header>
+                <section className={styles.body}>
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                </section>
+                <footer className={styles.footer}>
+                    <Avatar
+                        imageSrc={post.author.avatar}
+                        name={post.author.name}
                     />
-                </figure>
-            </header>
-            <section className={styles.body}>
-                <h2>{post.title}</h2>
-                <p>{post.body}</p>
-            </section>
-            <footer className={styles.footer}>
-                <Avatar
-                    imageSrc={post.author.avatar}
-                    name={post.author.name}
-                />
-            </footer>
-        </article>
+                </footer>
+            </article>
+        </Link>
     )
 }

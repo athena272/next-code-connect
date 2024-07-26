7caf7b5a243c5d6c9f01dbbb0b7beca85facb3de
athena272 <guilhermera272@gmail.com>
2024-07-25 22:38:45 -0300

    Update functions.tsx

--- src/helper/functions.tsx
+++ src/helper/functions.tsx
@@ -1,6 +1,11 @@
 // import logger from '@/logger'
+import { Post } from "@/types/Post";
 
-export async function getAllPosts() {
+type PostResponse = {
+    posts: Post[];
+}
+
+export async function getAllPosts(): Promise<Post[]> {
     try {
         const response = await fetch('https://athena272.github.io/next-code-connect/posts.json')
         if (!response.ok) {
@@ -8,10 +13,28 @@ export async function getAllPosts() {
         }
 
         console.log('Posts obtidos com sucesso')
-        const { posts } = await response.json();
+        const { posts }: PostResponse = await response.json();
         return posts
     } catch (error: any) {
         console.log('Ops, algo correu mal: ' + error.message)
         return []
+    }
+}
+
+export async function getPostsBySlug(slug: string) {
+    try {
+        const posts = await getAllPosts();
+        const post = posts.find((post) => post.slug === slug);
+
+        if (!post) {
+            console.log('Post não encontrado');
+            return null;
+        }
+
+        console.log('Post obtido com sucesso');
+        return post;
+    } catch (error: any) {
+        console.log('Ops, algo correu mal: ' + error.message);
+        return null;
     }
 }

9d2696380d8c4412e82688920ff7f711e5b45872
athena272 <guilhermera272@gmail.com>
2024-07-28 21:37:06 -0300

    Update error.tsx

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -23,7 +23,7 @@ export default function Error({
             <Image src={banner} alt='Erro ao encontrar os posts'/>
             <Heading>Opa! Ocorreu um erro.</Heading>
             <p className={styles.text}>Não conseguimos carregar a página, volte para seguir navegando.</p>
-            <Link href="/">
+            <Link href="/" passHref>
                 Voltar ao feed <ArrowBack color='#81FE88' />
             </Link>
         </div>

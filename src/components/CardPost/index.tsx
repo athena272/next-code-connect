d921c606d80e607d9b779489ad515efdbcab248d
athena272 <guilhermera272@gmail.com>
2024-12-05 15:33:49 -0300

    Update index.tsx

--- src/components/CardPost/index.tsx
+++ src/components/CardPost/index.tsx
@@ -2,13 +2,15 @@ import Image from "next/image"
 import Avatar from "../Avatar"
 import { Post } from "@/types/Post"
 import styles from './CardPost.module.scss'
+import Link from "next/link"
 
 type CardPostProps = {
     post: Post,
-    highlight?: boolean
+    highlight?: boolean,
+    showBtnDetails?: boolean,
 }
 
-export default function CardPost({ post, highlight }: CardPostProps) {
+export default function CardPost({ post, highlight, showBtnDetails }: CardPostProps) {
     return (
         <article className={styles.card} style={{ width: highlight ? 993 : 486 }}>
             <header className={`${styles.header} ${!highlight ? styles.header__home : ''}`}>
@@ -23,6 +25,12 @@ export default function CardPost({ post, highlight }: CardPostProps) {
             <section className={styles.body}>
                 <h2>{post.title}</h2>
                 <p>{post.body}</p>
+                {
+                    showBtnDetails ?
+                        <Link className={styles.link} href={`/posts/${post.slug}`}>Ver detalhes</Link>
+                        :
+                        null
+                }
             </section>
             <footer className={styles.footer}>
                 <Avatar

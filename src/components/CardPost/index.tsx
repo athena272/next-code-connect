04a4cfdde13a2309c93acdbeaa9dadcbec178956
athena272 <guilhermera272@gmail.com>
2024-12-05 17:30:08 -0300

    Update index.tsx

--- src/components/CardPost/index.tsx
+++ src/components/CardPost/index.tsx
@@ -3,6 +3,9 @@ import Avatar from "../Avatar"
 import { Post } from "@/types/Post"
 import styles from './CardPost.module.scss'
 import Link from "next/link"
+import { IconButton } from "../IconButton"
+import ThumbsUp from "../icons/ThumbsUp"
+import incrementThumbsUp from "@/actions"
 
 type CardPostProps = {
     post: Post,
@@ -11,6 +14,9 @@ type CardPostProps = {
 }
 
 export default function CardPost({ post, highlight, showBtnDetails }: CardPostProps) {
+
+    const submitThumbsUp = incrementThumbsUp.bind(null, post)
+
     return (
         <article className={styles.card} style={{ width: highlight ? 993 : 486 }}>
             <header className={`${styles.header} ${!highlight ? styles.header__home : ''}`}>
@@ -33,6 +39,16 @@ export default function CardPost({ post, highlight, showBtnDetails }: CardPostPr
                 }
             </section>
             <footer className={styles.footer}>
+                <div>
+                    <form action={submitThumbsUp}>
+                        <IconButton>
+                            <ThumbsUp />
+                        </IconButton>
+                    </form>
+                    <p>
+                        {post.likes}
+                    </p>
+                </div>
                 <Avatar
                     imageSrc={post.author.avatar}
                     name={post.author.name}

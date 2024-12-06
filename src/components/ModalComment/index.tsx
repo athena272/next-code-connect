34a7995ec3df1a67888cde3b85493ce8bc1a1ee0
athena272 <guilhermera272@gmail.com>
2024-12-06 09:04:33 -0300

    Update index.tsx

--- src/components/ModalComment/index.tsx
+++ src/components/ModalComment/index.tsx
@@ -4,14 +4,30 @@ import { useRef } from "react";
 import Modal, { ModalRef } from "../Modal";
 import { IconButton } from "../IconButton";
 import Chat from "../icons/Chat";
+import { Subheading } from "../Subheading";
+import { Textarea } from "../Textarea";
+import styles from './ModalComment.module.scss'
+import SubmitButton from "../SubmitButton";
 
-export default function ModalComment() {
+type ModalCommentProps = {
+    action: (formData: FormData) => Promise<void>;
+};
+
+export default function ModalComment({ action }: ModalCommentProps) {
     const modalRef = useRef<ModalRef>(null)
 
     return (
         <>
             <Modal ref={modalRef}>
-                <h1>Olá mundo!</h1>
+                <form action={action} onSubmit={() => modalRef.current?.closeModal()}>
+                    <Subheading>Deixe seu comentário sobre o post:</Subheading>
+                    <Textarea required rows={8} name="text" placeholder="Digite aqui..." />
+                </form>
+                <div className={styles.footer}>
+                    <SubmitButton>
+                        Comentar
+                    </SubmitButton>
+                </div>
             </Modal>
             <IconButton
                 onClick={() => modalRef.current?.openModal()}

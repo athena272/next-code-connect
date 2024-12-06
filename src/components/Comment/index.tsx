172c4b8a907c40d26a7f39ae8ee6e96957bbd83c
athena272 <guilhermera272@gmail.com>
2024-12-06 10:36:56 -0300

    Update index.tsx

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -1,14 +1,14 @@
 import { Comment as CommentType } from "@/types/Comment";
 import Image from "next/image";
+import styles from './Comment.module.scss'
 
 type CommentProps = {
     comment: CommentType
 }
 
-
 export default function Comment({ comment }: CommentProps) {
     return (
-        <div>
+        <div className={styles.comment}>
             <Image
                 src={comment.author.avatar}
                 width={32}

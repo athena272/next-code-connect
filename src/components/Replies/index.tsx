a80da1da55d4cfbfa3c69ebe4fd367c7067942ec
athena272 <guilhermera272@gmail.com>
2024-12-06 15:13:00 -0300

    Update index.tsx

--- src/components/Replies/index.tsx
+++ src/components/Replies/index.tsx
@@ -1,10 +1,11 @@
 'use client'
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import styles from './Replies.module.scss'
 import { Comment as CommentType } from "@/types/Comment"
 import Comment from "../Comment"
 import ModalReplay from "../ModalReply"
+import Spinner from "../Spinner"
 
 type RepliesProps = {
     comment: CommentType
@@ -12,6 +13,30 @@ type RepliesProps = {
 
 export default function Replies({ comment }: RepliesProps) {
     const [showReplies, setShowReplies] = useState(false)
+    const [commentsReplay, setCommentsReplay] = useState<CommentType[]>([])
+    const [isLoading, setIsLoading] = useState(false); // Estado para rastrear carregamento
+
+    async function fetchData() {
+        setIsLoading(true); // Inicia o spinner
+        try {
+            const response = await fetch(`/api/comment/${comment.id}/replies`);
+            if (!response.ok) {
+                throw new Error("Failed to fetch replies");
+            }
+            const data: CommentType[] = await response.json();
+            setCommentsReplay(data);
+        } catch (error) {
+            console.error("Error fetching replies:", error);
+        } finally {
+            setIsLoading(false); // Finaliza o spinner
+        }
+    }
+
+    useEffect(() => {
+        if (showReplies) {
+            fetchData()
+        }
+    }, [showReplies])
 
     return (
         <div className={styles.replies}>
@@ -22,17 +47,27 @@ export default function Replies({ comment }: RepliesProps) {
                 {showReplies ? 'Ocultar' : 'Ver'} respostas
             </button>
             {
-                showReplies &&
-                <ul>
-                    {
-                        comment.children.map(commentReply => (
-                            <li key={commentReply.id}>
-                                <Comment comment={commentReply} />
-                                <ModalReplay comment={commentReply} />
-                            </li>
-                        ))
-                    }
-                </ul>
+                showReplies && (
+                    <>
+                        {
+                            isLoading ?
+                                <Spinner />
+                                :
+                                (
+                                    <ul>
+                                        {
+                                            commentsReplay.map(commentReply => (
+                                                <li key={commentReply.id}>
+                                                    <Comment comment={commentReply} />
+                                                    <ModalReplay comment={commentReply} />
+                                                </li>
+                                            ))
+                                        }
+                                    </ul>
+                                )
+                        }
+                    </>
+                )
             }
         </div>
     )

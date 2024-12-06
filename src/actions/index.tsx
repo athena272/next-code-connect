4bab2431dc749464d7f3834dbc6b71b6c97fde59
athena272 <guilhermera272@gmail.com>
2024-12-06 09:03:36 -0300

    Update index.tsx

--- src/actions/index.tsx
+++ src/actions/index.tsx
@@ -4,7 +4,7 @@ import { Post } from "@/types/Post";
 import db from "../../prisma/db";
 import { revalidatePath } from "next/cache";
 
-export default async function incrementThumbsUp(post: Post) {
+export async function incrementThumbsUp(post: Post) {
 
     // await new Promise((resolve, reject) => setTimeout(resolve, 1500))
 
@@ -16,6 +16,34 @@ export default async function incrementThumbsUp(post: Post) {
             likes: {
                 increment: 1
             }
+        }
+    })
+
+    revalidatePath('/')
+    revalidatePath(`/${post.slug}`)
+}
+
+export async function postComment(post: Post, formData: FormData) {
+    const author = await db.user.findFirst({
+        where: {
+            username: 'anabeatriz_dev'
+        }
+    })
+
+    if (!author) {
+        throw new Error('Author not found');
+    }
+
+    const text = formData.get('text');
+    if (typeof text !== 'string') {
+        throw new Error('Invalid text');
+    }
+
+    await db.comment.create({
+        data: {
+            text,
+            authorId: author.id, // aqui não será mais undefined pois checamos if (!author)
+            postId: post.id,
         }
     })
 

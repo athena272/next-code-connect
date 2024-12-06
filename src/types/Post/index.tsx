043751ef517767df4739bff36138d0bc628f978e
athena272 <guilhermera272@gmail.com>
2024-12-06 16:39:07 -0300

    Update index.tsx

--- src/types/Post/index.tsx
+++ src/types/Post/index.tsx
@@ -1,18 +1,22 @@
 import { Comment } from "../Comment"
 
 export type Post = {
-    id: number,
-    cover: string,
-    title: string,
-    slug: string,
-    body: string,
-    markdown: string,
+    id: number;
+    cover: string;
+    title: string;
+    slug: string;
+    body: string;
+    markdown: string;
     author: {
-        id: number,
-        name: string,
-        username: string,
-        avatar: string
-    },
-    likes: number,
-    comments: Comment[] // Post has many comments
-}
+        id: number;
+        name: string;
+        username: string;
+        avatar: string;
+    };
+    comments: Comment[];
+    likes: number;
+    // Campos extras do Prisma
+    createdAt?: Date;
+    updatedAt?: Date;
+    authorId?: number;
+};
